37e9b545f9926ddc7cb7c0bf7d9ed138e4ae6c08
telesouttax <lucas.teles@outtax.com.br>
2026-06-11 11:45:58 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,23 @@
+import CopyPlugin from "copy-webpack-plugin";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.resolve.alias.canvas = false;
+
+    if (!isServer) {
+      config.plugins.push(
+        new CopyPlugin({
+          patterns: [
+            {
+              from: "node_modules/pdfjs-dist/build/pdf.worker.min.mjs",
+              to: "../public/pdf.worker.min.js",
+            },
+          ],
+        })
+      );
+    }
+
     return config;
   },
 };
